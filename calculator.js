bcb9fba307eecaed83f4a53dde0c5db757a0b864
rusheelkurapati30 <62334199+rusheelkurapati30@users.noreply.github.com>
2020-10-29 11:22:36 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,64 @@
+function seven()
+{
+	calculator.display.value+='7';
+}
+function eight()
+{
+	calculator.display.value+='8';
+}
+function nine()
+{
+	calculator.display.value+='9';
+}
+function multiplication()
+{
+	calculator.display.value+='*';
+}
+function four()
+{
+	calculator.display.value+='4';
+}
+function five()
+{
+	calculator.display.value+='5';
+}
+function six()
+{
+	calculator.display.value+='6';
+}
+function subtraction()
+{
+	calculator.display.value+='-';
+}
+function one()
+{
+	calculator.display.value+='1';
+}
+function two()
+{
+	calculator.display.value+='2';
+}
+function three()
+{
+	calculator.display.value+='3';
+}
+function addition()
+{
+	calculator.display.value+='+';
+}
+function clear()
+{
+	calculator.display.value+='';
+}
+function zero()
+{
+	calculator.display.value+='0';
+}
+function result()
+{
+	calculator.display.value=eval(calculator.display.value);
+}
+function division()
+{
+	calculator.display.value+='/';
+}
